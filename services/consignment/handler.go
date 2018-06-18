3d1c1d77b1a835b992a9c577fc5d0e0908c6d380
Hassan Chabab <chababhassan@gmail.com>
2018-06-18 23:35:36 +0000

    Renamed handler type to service one

--- services/consignment/handler.go
+++ services/consignment/handler.go
@@ -15,19 +15,19 @@ import (
 // we defined in our protobuf definition. You can check the interface
 // in the generated code itself for the exact method signatures etc
 // to give you a better idea.
-type handler struct {
-	vesselClient vesselProto.VesselService
+type service struct {
 	session      *mgo.Session
+	vesselClient vesselProto.VesselService
 }
 
-func (s *handler) GetRepo() Repository {
+func (s *service) GetRepo() Repository {
 	return &ConsignmentRepository{s.session.Clone()}
 }
 
 // CreateConsignment - we created just one method on our service,
 // which is a create method, which takes a context and a request as an
 // argument, these are handled by the gRPC server.
-func (s *handler) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
+func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
 	repo := s.GetRepo()
 	defer repo.Close()
 	// Here we call a client instance of our vessel service with our consignment weight,
@@ -58,7 +58,7 @@ func (s *handler) CreateConsignment(ctx context.Context, req *pb.Consignment, re
 	return nil
 }
 
-func (s *handler) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
+func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
 	repo := s.GetRepo()
 	defer repo.Close()
 	consignments, err := repo.GetAll()
